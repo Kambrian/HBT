f7b14549436c4f1c31c36ba41be7bbbfbbcd2888
Jiaxin Han <hanjiaxin@gmail.com>
2017-02-15 18:52:32 +0900

    test for large memory

--- toolbox/Check/testll.c
+++ toolbox/Check/testll.c
@@ -9,71 +9,73 @@
 #include "iovars.h"
 #include "proto.h"
 
-// #define NDIV 40
 // #define np 500
-#define NDIV 3
+#define NDIV 256
 #undef PERIODIC_BDR
-#include "linkedlist.c"
+// #include "linkedlist.c"
 
-int print_cell(int id, LINKLIST *l);
-int print_cell2(int id);
+HBTInt print_cell(HBTInt id, LINKLIST *l);
+// HBTInt print_cell2(HBTInt id);
 int main(int argc, char** argv)
 {	
 	logfile=stdout;//redirect BT routines' log info to standard output	
 
-	int np=atoi(argv[1]);
-// 	int ndiv=NDIV;
-	int id=0;
+	HBTInt np=NP_DM;// atoi(argv[1]);
+// 	HBTInt ndiv=NDIV;
+	HBTInt id=0;
 	
-	HBTxyz Pos=mymalloc(sizeof(HBTxyz)*np);
-	int i,j;
+	HBTxyz *Pos=calloc(np*3, sizeof(HBTReal));
+	HBTInt i,j;
+	/*
+#pragma omp parallel for
 	for(i=0;i<np;i++)
 	{
 	  for(j=0;j<3;j++)
 	    Pos[i][j]=drand48()*2;
 	}
-	makell(Pos, np, NDIV); 
+	*/
+// 	makell(Pos, np, NDIV); 
 	
 	LINKLIST l, l2;
-	make_linklist(&l, np,NDIV, Pos, GetArrPos, 0);
-	omp_set_num_threads(1);
-	make_linklist(&l2, np,NDIV, Pos, GetArrPos, 0);
+	make_linklist(&l, np,NDIV, Pos, GetArrPos, 1);
+// 	omp_set_num_threads(1);
+// 	make_linklist(&l2, np,NDIV, Pos, GetArrPos, 0);
 	
 	print_cell(id, &l);
 	print_cell(id, &l2);
-	print_cell2(id);
+// 	print_cell2(id);
 	
 	free_linklist(&l);
-	free_linklist(&l2);
+// 	free_linklist(&l2);
 	myfree(Pos);
 	
 	return 0;
 }
 
-int print_cell(int id, LINKLIST *l)
+HBTInt print_cell(HBTInt id, LINKLIST *l)
 {
-  int n=0;
-  int p=l->hoc[id];
+  HBTInt n=0;
+  HBTInt p=l->hoc[id];
   while(p>=0)
   {
-    printf("%d ", p);
+//     printf("%d ", p);
     p=l->list[p];
     n++;
   }
-  printf("\n%d particles\n", n);
+  printf("\n%ld particles\n", n);
   return n;
 }
-
-int print_cell2(int id)
+/*
+HBTInt print_cell2(HBTInt id)
 {
-  int n=0;
-  int p=hoc[0][0][0];
+  HBTInt n=0;
+  HBTInt p=hoc[0][0][0];
   while(p>=0)
   {
-    printf("%d ", p);
+//     printf("%d ", p);
     p=ll[p];
     n++;
   }
-  printf("\n%d particles\n", n);
+  printf("\n%ld particles\n", n);
   return n;
-}
+}*/
